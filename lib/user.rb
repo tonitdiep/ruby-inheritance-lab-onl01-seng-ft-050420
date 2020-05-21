9fb9fe3c32ff0b7fabf0de2eccfd34af84b3395a
Toni  Diep <adiep123@gmail.com>
2020-05-21 04:17:01 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -7,8 +7,8 @@ jim = User.new
 steve = User.new
 avi = User.new
 # jim = Jim.new
-# jim.first_name = "Jim"
-# jim.last_name = "Jim"
-# some_knowledge = avi.teach 
+jim.first_name = "Jim"
+jim.last_name = "Jim"
+some_knowledge = avi.teach 
 # jim.learn(some_knowledge)
 # puts "Jim just learned this important knowledge: '#{jim.knowledge.first}' from Avi"
