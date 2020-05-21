f821e13682086b5907e13d02a70a89e16c555bb3
Toni  Diep <adiep123@gmail.com>
2020-05-21 04:12:52 +0000

    Done.

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,14 @@
 class User
   attr_accessor :first_name, :last_name
+  
 end
+
+jim = User.new
+steve = User.new
+avi = User.new
+# jim = Jim.new
+# jim.first_name = "Jim"
+# jim.last_name = "Jim"
+# some_knowledge = avi.teach 
+# jim.learn(some_knowledge)
+# puts "Jim just learned this important knowledge: '#{jim.knowledge.first}' from Avi"
